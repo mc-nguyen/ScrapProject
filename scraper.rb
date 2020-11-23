b0a04361937f7d9a66421c05f9b2d636b71ec4f3
Vy Nguyen <gaquangvy@gmail.com>
2020-11-22 19:21:49 -0800

    Initial project with parsing a page from http://books.toscrape.com/

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,15 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
+require 'json'
 
 class Scraper
   def initialize
     @url = "http://books.toscrape.com/"
     @doc = Nokogiri::HTML(HTTParty.get(@url))
+    @total_item = @doc.css('strong').children[0].text.to_i
+    @book_list = @doc.css('ol.row')
+
     byebug
   end
 

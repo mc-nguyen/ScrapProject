01580ba4339bd0bb4f463084e7ef45290b5f06de
gaquangvy <vqn@cpp.edu>
2020-11-23 16:47:13 -0800

    adding threads

--- scraper.rb
+++ scraper.rb
@@ -1,55 +1,61 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
 require 'httparty'
-require 'byebug'
 require 'open-uri'
 require 'csv'
 
 class Scraper
+  @@categories = 0
   def initialize
-    @url = "http://books.toscrape.com/"
-    @html = open(@url).read
+    @url = 'http://books.toscrape.com/'
+    @html = URI.open(@url).read
     @doc = Nokogiri::HTML(@html)
-    @total_page = @doc.search("li.current")[0].children[0].text.strip!.split(' ')[3].to_i
+    @total_page = @doc.search('li.current')[0].children[0].text.strip!.split(' ')[3].to_i
     @book_list = []
-    @total_page.times do |i|
-      url = @url + "catalogue/page-#{i+1}.html"
-      html = open(url).read
-      doc = Nokogiri::HTML(html)
-      doc.search("article.product_pod").each do |ele|
-        book = {
-            "img" => @url + ele.css("img.thumbnail")[0].attributes["src"].value,
-            "rate" => convert(ele.css('p.star-rating')[0].attributes["class"].value.split(' ')[1]),
-            "title" => ele.css("h3")[0].children[0].attributes["title"].value,
-            "link" => @url + ele.css("h3")[0].children[0].attributes["href"].value,
-            "price in £" => ele.css("p.price_color").text.tr('£','').to_f,
-            "in stock" => ele.css("p.instock.availability").text.strip! == "In stock"
-        }
-        @book_list << book
-      end
-    end
-    count = 1
-    @book_list.each do |ele|
-      puts "Book #{count}:"
-      ele.each do |category, value|
-        puts "\t#{category}: #{value}"
-      end
-      count += 1
-    end
 
-    byebug
+    current = Time.now
+    threads = []
+    @total_page.times do |i|
+      url = @url + "catalogue/page-#{i + 1}.html"
+      def collectBooks(url)
+        html = URI.open(url).read
+        doc = Nokogiri::HTML(html)
+        doc.search('article.product_pod').each do |ele|
+          book = {
+            'img' => @url + ele.css('img.thumbnail')[0].attributes['src'].value,
+            'rate' => convert(ele.css('p.star-rating')[0].attributes['class'].value.split(' ')[1]),
+            'title' => ele.css('h3')[0].children[0].attributes['title'].value,
+            'link' => @url + ele.css('h3')[0].children[0].attributes['href'].value,
+            'price in £' => ele.css('p.price_color').text.tr('£', '').to_f,
+            'in stock' => ele.css('p.instock.availability').text.strip! == 'In stock'
+          }
+          @book_list << book
+        end
+      end
+      threads << Thread.new { collectBooks(url) }
+    end
+    threads.each(&:join)
+    current = Time.now - current
+    puts "Time execution: #{current}"
+
+    @@categories += 1
   end
 
   def convert(wordNumber)
     translate = {
-        "One" => 1,
-        "Two" => 2,
-        "Three" => 3,
-        "Four" => 4,
-        "Five" => 5
+      'One' => 1,
+      'Two' => 2,
+      'Three' => 3,
+      'Four' => 4,
+      'Five' => 5
     }
-    return translate[wordNumber]
+    translate[wordNumber]
   end
 
+  def getCategories
+    return @@categories
+  end
 end
 
-scrap = Scraper.new
+scrape = Scraper.new
